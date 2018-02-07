b0e9e0567638706dd0ac600f371468bcc722de69
Spicely <Spicely@outlook.com>
2018-02-07 18:26:19 +0800

    []

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -8,25 +8,23 @@ class Component extends React.Component<any, any> {
         this.slots.map((item) => {
             this.slotsNode[item] = []
         })
+        this.slotsNode['default'] = []
     }
     readonly createSlots = () => {
         this.setSlots()
         let children = this.props.children instanceof Array ? this.props.children : [this.props.children]
         children.map((item: JSX.Element) => {
-            let slot = item && item.props.slot
-            if (this.slotsNode.hasOwnProperty(slot) && slot) {
+            let slot = item && item.props && item.props.slot
+            if (slot && this.slotsNode.hasOwnProperty(slot)) {
                 this.slotsNode[slot].push(item)
             } else {
-                this.slotsNode['default'].push(item)
+                item && this.slotsNode['default'].push(item)
             }
         })
     }
     className : string = ''
-    static defaultProps = {
-        className: ''
-    }
     getClassName() {
-        return `${this.className || ''} ${this.props.className || ''}`
+        return `${this.className} ${this.props.className || ''}`
     }
 }
 export {Component, React}
